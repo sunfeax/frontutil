219a2cdef549351447a2b2b3d2878c211d64b205
sunfeax <sunfeax@gmail.com>
2025-11-25 11:26:50 +0100

    toggle state publication

--- src/app/component/uski/components/registro-card-private/registro-card-private.component.ts
+++ src/app/component/uski/components/registro-card-private/registro-card-private.component.ts
@@ -1,13 +1,25 @@
 import { Component, Input } from '@angular/core';
 import { IVisita } from '../../types/visitas';
+import { CommonModule } from '@angular/common';
+import { VisitasService } from '../../services/visitas';
 
 @Component({
   selector: 'app-registro-card-private',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './registro-card-private.component.html',
   styleUrl: './registro-card-private.component.css',
   standalone: true
 })
 export class RegistroCardPrivateComponent {
+  constructor(private oVisitasService: VisitasService) {}
+
   @Input() oVisita!: IVisita;
+
+  onTogglePublish(visita: IVisita) {
+    const nuevoEstado = !visita.estaPublicado;
+    this.oVisitasService.update({ id: visita.id, estaPublicado: nuevoEstado}).subscribe({
+      next: () => visita.estaPublicado = nuevoEstado,
+      error: (err) => console.error('No se pudo actualizar', err)
+    });
+  }
 }
